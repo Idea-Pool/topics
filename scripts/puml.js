d939b95a134178267b1946dc02aae1012d75b9fd
Laszlo Szikszai <laszlo_szikszai@epam.com>
2023-03-09 08:39:47 +0100

    Updated scripts

--- scripts/puml.js
+++ scripts/puml.js
@@ -2,15 +2,16 @@ const fs = require('fs');
 const { default: fetch } = require('node-fetch');
 
 const BASE = 'docs/uml/';
+const IMAGE = 'svg';
 
 const files = fs.readdirSync(BASE).filter(f => /\.puml$/.test(f));
 (async () => {
   for (const f of files) {
     console.log('Processing', BASE + f);
-    const out = f.replace('.puml', '.svg');
+    const out = f.replace('.puml', '.' + IMAGE);
     const puml = fs.readFileSync(BASE + f, 'utf-8');
-    const response = await fetch('http://www.plantuml.com/plantuml/svg/~h' + Buffer.from(puml, 'utf-8').toString('hex'));
-    fs.writeFileSync(BASE + out, await response.text());
+    const response = await fetch('http://www.plantuml.com/plantuml/' + IMAGE + '/~h' + Buffer.from(puml, 'utf-8').toString('hex'));
+    response.body.pipe(fs.createWriteStream(BASE + out));
     console.log('Added', BASE + out);
   }
 })();
