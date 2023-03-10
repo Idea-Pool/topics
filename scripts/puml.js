9a34c18adb7c6295f2f615962ad7d12f2434b185
Laszlo Szikszai <laszlo_szikszai@epam.com>
2023-03-10 15:39:52 +0100

    Updated process and husky scripts

--- scripts/puml.js
+++ scripts/puml.js
@@ -2,16 +2,22 @@ const fs = require('fs');
 const { default: fetch } = require('node-fetch');
 
 const BASE = 'docs/uml/';
-const IMAGE = 'svg';
+
+async function convertPuml(file, format) {
+  const out = file.replace('.puml', '.' + format);
+  const puml = fs.readFileSync(BASE + file, 'utf-8');
+  const response = await fetch(
+    'http://www.plantuml.com/plantuml/' + format + '/~h' + Buffer.from(puml, 'utf-8').toString('hex')
+  );
+  response.body.pipe(fs.createWriteStream(BASE + out));
+  return BASE + out;
+}
 
 const files = fs.readdirSync(BASE).filter(f => /\.puml$/.test(f));
 (async () => {
   for (const f of files) {
     console.log('Processing', BASE + f);
-    const out = f.replace('.puml', '.' + IMAGE);
-    const puml = fs.readFileSync(BASE + f, 'utf-8');
-    const response = await fetch('http://www.plantuml.com/plantuml/' + IMAGE + '/~h' + Buffer.from(puml, 'utf-8').toString('hex'));
-    response.body.pipe(fs.createWriteStream(BASE + out));
-    console.log('Added', BASE + out);
+    console.log('Added', await convertPuml(f, 'svg'));
+    console.log('Added', await convertPuml(f, 'png'));
   }
 })();
