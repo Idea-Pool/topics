05bed79e0da486c264418f7adf357965eb02213a
Laszlo Szikszai <laszlo_szikszai@epam.com>
2023-03-17 13:17:58 +0100

    Optimized PUML generation

--- scripts/puml.js
+++ scripts/puml.js
@@ -1,23 +1,97 @@
 const fs = require('fs');
-const { default: fetch } = require('node-fetch');
+const crypto = require('crypto');
+const path = require('path');
+const {default: fetch} = require('node-fetch');
 
 const BASE = 'docs/uml/';
 
-async function convertPuml(file, format) {
-  const out = file.replace('.puml', '.' + format);
-  const puml = fs.readFileSync(BASE + file, 'utf-8');
-  const response = await fetch(
-    'http://www.plantuml.com/plantuml/' + format + '/~h' + Buffer.from(puml, 'utf-8').toString('hex')
-  );
-  response.body.pipe(fs.createWriteStream(BASE + out));
-  return BASE + out;
+class HashStore {
+    constructor() {
+        if (fs.existsSync(this.HASH_STORE_FILE)) {
+            this.hashes = JSON.parse(fs.readFileSync(this.HASH_STORE_FILE, 'utf-8'));
+        } else {
+            this.hashes = {};
+        }
+    }
+
+    get HASH_STORE_FILE() {
+        return path.join(__dirname, '..', '.hash.json');
+    }
+
+    hashFile(file) {
+        const fileBuffer = fs.readFileSync(file);
+        const hashSum = crypto.createHash('sha256');
+        hashSum.update(fileBuffer);
+        const hex = hashSum.digest('hex');
+        if (hex === this.hashes[file]) {
+            return false;
+        }
+        this.hashes[file] = hex;
+        return true;
+    }
+
+    flush() {
+        fs.writeFileSync(
+            this.HASH_STORE_FILE,
+            JSON.stringify(this.hashes, null, 2),
+            'utf-8',
+        );
+    }
+}
+
+class PUMLConverter {
+    constructor(file, hashStore) {
+        this.sourceFile = file;
+        this.updated = hashStore.hashFile(file);
+    }
+
+    get png() {
+        return this.sourceFile.replace('.puml', '.png');
+    }
+
+    get svg() {
+        return this.sourceFile.replace('.puml', '.svg');
+    }
+
+    get puml() {
+        return fs.readFileSync(this.sourceFile, 'utf-8');
+    }
+
+    get hex() {
+        return Buffer.from(this.puml, 'utf-8').toString('hex');
+    }
+
+    async toPNG() {
+        return this._convertTo('png');
+    }
+
+    async toSVG() {
+        return this._convertTo('svg');
+    }
+
+    async _convertTo(type) {
+        if (this.updated || !fs.existsSync(this[type])) {
+            const response = await fetch(`http://www.plantuml.com/plantuml/${type}/~h${this.hex}`);
+            response.body.pipe(fs.createWriteStream(this[type]));
+            console.log('Added', this[type]);
+        } else {
+            console.log('Skipping (no change)', this[type]);
+        }
+    }
+
+    async convert() {
+        console.log('Processing', this.sourceFile);
+        await this.toPNG();
+        await this.toSVG();
+    }
 }
 
 const files = fs.readdirSync(BASE).filter(f => /\.puml$/.test(f));
 (async () => {
-  for (const f of files) {
-    console.log('Processing', BASE + f);
-    console.log('Added', await convertPuml(f, 'svg'));
-    console.log('Added', await convertPuml(f, 'png'));
-  }
+    const hashStore = new HashStore();
+    for (const f of files) {
+        const converter = new PUMLConverter(BASE + f, hashStore);
+        await converter.convert();
+    }
+    hashStore.flush();
 })();
