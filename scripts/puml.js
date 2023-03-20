7d110461ffb422c5d67a6793bcb9ba0654f1114b
Laszlo Szikszai <laszlo_szikszai@epam.com>
2023-03-20 15:10:16 +0100

    Updated theme logic

--- scripts/puml.js
+++ scripts/puml.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const puml = require('node-plantuml');
 const {default: fetch} = require('node-fetch');
 const {HashStore} = require('./hash_store');
@@ -19,11 +20,19 @@ function encodePuml(input) {
     });
 }
 
-
 class PUMLConverter {
     constructor(file, hashStore) {
         this.sourceFile = file;
         this.updated = hashStore.hashFile(file);
+    }
+
+    parse() {
+        const pumlContent = fs.readFileSync(this.sourceFile, 'utf-8');
+        const THEME_RX = /^!theme (\S+) from (\S+)$/gmi;
+        return pumlContent.replace(THEME_RX, (_, theme, dir) => {
+            const themePath = path.join(BASE, dir, `puml-theme-${theme}.puml`);
+            return fs.readFileSync(themePath, 'utf-8');
+        });
     }
 
     static readFiles(directory) {
@@ -53,7 +62,7 @@ class PUMLConverter {
 
     async _convertTo(type) {
         if (this._needToGenerate(type)) {
-            const gen = await encodePuml(this.sourceFile);
+            const gen = await encodePuml(this.parse());
             const response = await fetch(`http://www.plantuml.com/plantuml/${type}/${gen}`);
             response.body.pipe(fs.createWriteStream(this[type]));
             console.log('Added', this[type]);
