04e90083a7ad9dc07ea51a5927458649abb0bfeb
Szikszai László <sziklaszlo@gmail.com>
2023-06-09 16:50:41 +0200

    Update add-task.js

--- tasks/scripts/add-task.js
+++ tasks/scripts/add-task.js
@@ -1,6 +1,41 @@
 const tasks = require('../tasks.json');
 
+function processTaskList(issue, body, taskList) {
+  if (!issue.labels.find(label => label.name === taskList.label)) {
+    console.log("No match");
+    return body;
+  }
+  const START_TOKEN = `<!-- start:task-list:${taskList.id} -->`;
+  const END_TOKEN = `<!-- end:task-list:${taskList.id} -->`;
+  if (body.includes(START_TOKEN)) {
+    console.log("Task list already added");
+    return body;
+  }
+  console.log("Adding task list");
+  return [
+    body,
+    "## " + (taskList.title || `Tasks (${taskList.id})`),
+    "",
+    ...taskList.tasks.map(item => `- [ ] ${item}`),
+  ].join("\r\n");
+}
+
 module.exports = ({github, context}) => {
+  const activeTaskLists = Object.keys(tasks)
+    .filter(id => tasks[id].enabled)
+    .map(id => ({ ...tasks[id], id }));
+  
   const issue = context.payload.issue;
-  console.log({issue});
+  let body = issue.body;
+  
+  for (const taskList of activeTaskLists) {
+    console.log("Processing task list:", taskList.id);
+    body = processTaskList(issue, body, taskList);
+  }
+  
+  if (body != issue.body) {
+    console.log("Updating issue");
+  } else {
+    console.log("No change");
+  }
 }
